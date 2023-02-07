09ca64f289cc4872077130cb91dd93db9637e569
Zeheng Li <imzehengl@gmail.com>
2023-02-07 15:30:38 -0700

    init

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
-
+from tmdb import TMDb3
 
 st.set_page_config(page_title="ezapi-tmdb-demo", page_icon="random")
+_, center, _ = st.columns([2, 1, 2])
+with center:
+    st.image(
+        "https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg",
+        use_column_width=True,
+    )
 st.title("ezapi-tmdb-demo")
 st.caption("A streamlit app to demo ezapi-tmdb")
+
+tmdb = TMDb3(st.secrets["api_key"])
